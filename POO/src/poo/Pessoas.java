14bd4bb8389636d9aaaa449d79140630f3072986
Pedro Henrique Fernandes de Oliveira <fernandesdeoliveira261@gmail.com>
2024-09-12 22:28:00 -0300

    12/09/24

---
+++ POO/src/poo/Pessoas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Pessoas {
+    String nome;
+    int idade;
+    double altura;
+    
+    public void apresentar() {
+        System.out.println("Ola, o meu nome e " + nome + ", eu tenho " + idade + " anos e " + altura + "cm  de altura");
+    }
+}
---
+++ POO/src/poo/main_Pessoas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo;
+
+/**
+ *
+ * @author Pedro
+ */
+public class main_Pessoas {
+
+
+    public static void main(String[] args) {
+        Pessoas p1 = new Pessoas();
+        p1.nome = "Walyson";
+        p1.idade = 26;
+        p1.altura = 1.85;
+        p1.apresentar();
+    }
+    
+}
